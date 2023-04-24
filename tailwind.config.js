d199cc487489e40706eb08f741a6375b7f34aec9
Marieke Malfait <malfait99+becode@gmail.com>
2023-04-24 16:49:00 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
