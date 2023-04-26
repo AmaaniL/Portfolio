3aa628a6f457d28754589df31051ab4c8fbb01f9
Marieke Malfait <malfait99+becode@gmail.com>
2023-04-26 10:05:28 +0200

    Add favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ["./src/**/*.{html,js}",
+  "./*.{html,js}",
   "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     screens: {
