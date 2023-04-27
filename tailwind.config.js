2f6368f22455177242dba859c89de4038292902c
AmaaniL <malfait99+github@gmail.com>
2023-04-27 16:11:08 +0200

    Merge branch 'main' of https://github.com/AmaaniL/CV

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+
   content: ["./src/*.{html,js}",
   "./*.{html,js}",
-  "./node_modules/tw-elements/dist/js/**/*.js",
+  "./node_modules/tw-elements/dist/js/*.js",
+
   "./node_modules/flowbite/**/*.js"],
-  
+
+  safelist: [
+    'animate-[fade-in_left_1s_ease-in-out]'
+  ],
+
   theme: {
     screens: {
 
@@ -15,7 +21,8 @@ module.exports = {
     },
   },
   plugins: [
+    
   require('flowbite/plugin'),
-  require("tw-elements/dist/plugin.cjs")],
-  darkMode: "class"
+  require("tw-elements/dist/plugin")],
+  darkMode: 'class',
 }
