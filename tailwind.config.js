acaebf32419c1662e0925927dfad682f83bc9035
Marieke Malfait <malfait99+becode@gmail.com>
2023-04-28 13:07:18 +0200

    Add responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,11 @@ module.exports = {
 
   theme: {
     screens: {
-
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
     extend: {
 
